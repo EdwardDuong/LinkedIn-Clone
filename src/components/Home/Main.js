5733cc3bb777d6fac9cd62f4070f674058441c4c
= <=>
2022-08-11 01:18:58 +1000

    adjust home page

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -2,10 +2,65 @@ import React from "react";
 import styled from "styled-components";
 
 const Main = (props) => {
-  return <Container>Main</Container>;
+  return (
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>video</span>
+          </button>
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: main;
+`;
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb (0 0 0 /15%), 0 0 0 rgb(0 0 0/20%);
+`;
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+  div {
+    button {
+      outline: none;
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.6);
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      fill: white;
+    }
+  }
 `;
 export default Main;
